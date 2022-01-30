44546c6fb125069c466502948499baf55e0e51dd
ankitsingh483 <78837102+ankitsingh483@users.noreply.github.com>
2022-01-30 17:52:44 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#ADD
+def add(x,y):
+	return x+y
+#SUB
+def sub(x,y):
+	return x-y
+#MUL
+def mul(x,y):
+	return x*y
+#DIV
+def div(x,y):
+	if y==0:
+		return error
+	else:
+		return x/y
+#SQU
+def square(x):
+	return x*x
+
